a5c077ee6fa64eb26a542e7f99e33a878ca12613
jojojugadero <jmapgrado@gmail.com>
2022-03-08 13:13:06 +0100

    commit

---
+++ PRINCIPAL/VISTA/includes/administradores/cabeceraAdministradores.php
@@ -0,0 +1,20 @@
+<?php
+
+echo "<h1>ASDETE, la unión hace la fuerza...</h1>";
+
+echo "<p><a href ='VISTA/index.php'>Volver</a></p>";
+
+echo "<p><a href ='index.php'>VolverMal</a></p>";
+
+
+
+
+?>
+        
+    
+
+
+
+
+
+
