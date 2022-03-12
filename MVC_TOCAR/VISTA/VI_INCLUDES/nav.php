78f21c1e43d522ca50ddae1323c1b19b987536f9
jojojugadero <jmapgrado@gmail.com>
2022-03-12 19:14:40 +0100

    commit

---
+++ MVC_TOCAR/VISTA/VI_INCLUDES/nav.php
@@ -0,0 +1,41 @@
+
+
+<?php
+$pagina = $_SERVER['PHP_SELF'];
+$arrayDir = preg_split('/\//',$pagina);
+$dirRoot = '/'.$arrayDir[1].'/';
+$incRoot = $_SERVER['DOCUMENT_ROOT'].$dirRoot;
+
+
+
+echo "<p style='font-size:large;'><h4>Empresas del sector</h4></p>
+
+<ul>
+  
+<li class='li_sidebar'><a href='<?php echo $dirRoot; ?>MVC_TOCAR\VISTA\VI_EMPRESAS\empresa1.php' class='enlace_sidebar'>Empresa 1</a></li>
+<li class='li_sidebar'><a href='<?php echo $dirRoot; ?>MVC_TOCAR\VISTA\VI_EMPRESAS\empresa2.php' class='enlace_sidebar'>Empresa 2</a></li>
+<li class='li_sidebar'><a href='<?php echo $dirRoot; ?>MVC_TOCAR\VISTA\VI_EMPRESAS\empresa3.php' class='enlace_sidebar'>Empresa 3</a></li>
+<li class='li_sidebar'><a href='<?php echo $dirRoot; ?>MVC_TOCAR\VISTA\VI_EMPRESAS\empresa4.php' class='enlace_sidebar'>Empresa 4</a></li>
+<li class='li_sidebar'><a href='<?php echo $dirRoot; ?>MVC_TOCAR\VISTA\VI_EMPRESAS\empresa5.php' class='enlace_sidebar'>Empresa 5</a></li>
+<li class='li_sidebar'><a href='<?php echo $dirRoot; ?>MVC_TOCAR\VISTA\VI_EMPRESAS\empresa6.php' class='enlace_sidebar'>Empresa 6</a></li>
+<li class='li_sidebar'><a href='<?php echo $dirRoot; ?>MVC_TOCAR\VISTA\VI_EMPRESAS\empresa7.php' class='enlace_sidebar'>Empresa 7</a></li>
+
+</ul>";
+
+echo "<div></div>";
+
+echo "<p style='font-size:large;'><h4>Somos Asdete</h4></p>
+
+<ul>
+
+<li class='li_sidebar'><a href='<?php echo $dirRoot; ?>MVC_TOCAR\VISTA\VI_UTILS\calendarios.php' class='enlace_sidebar'>Calendarios laborales</a></li>
+<li class='li_sidebar'><a href='<?php echo $dirRoot; ?>MVC_TOCAR\VISTA\VI_UTILS\empleo.php' class='enlace_sidebar'>Tu empleo</a></li>
+<li class='li_sidebar'><a href='<?php echo $dirRoot; ?>MVC_TOCAR\VISTA\VI_UTILS\formacion.php' class='enlace_sidebar'>Forma T</a></li>
+<li class='li_sidebar'><a href='<?php echo $dirRoot; ?>MVC_TOCAR\VISTA\VI_UTILS\prestamos.php' class='enlace_sidebar'>Empresa 4</a></li>
+<li class='li_sidebar'><a href='<?php echo $dirRoot; ?>MVC_TOCAR\VISTA\VI_UTILS\empresa5.php' class='enlace_sidebar'>Empresa 5</a></li>
+
+</ul>";
+
+
+
+?>
