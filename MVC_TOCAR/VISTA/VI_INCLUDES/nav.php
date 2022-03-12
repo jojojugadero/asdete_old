246a76510656792a6111af121489546332f3620c
jojojugadero <jmapgrado@gmail.com>
2022-03-12 19:23:42 +0100

    commit

--- MVC_TOCAR/VISTA/VI_INCLUDES/nav.php
+++ MVC_TOCAR/VISTA/VI_INCLUDES/nav.php
@@ -12,13 +12,13 @@ echo "<p style='font-size:large;'><h4>Empresas del sector</h4></p>
 
 <ul>
   
-<li class='li_sidebar'><a href='<?php echo $dirRoot; ?>MVC_TOCAR\VISTA\VI_EMPRESAS\empresa1.php' class='enlace_sidebar'>Empresa 1</a></li>
-<li class='li_sidebar'><a href='<?php echo $dirRoot; ?>MVC_TOCAR\VISTA\VI_EMPRESAS\empresa2.php' class='enlace_sidebar'>Empresa 2</a></li>
-<li class='li_sidebar'><a href='<?php echo $dirRoot; ?>MVC_TOCAR\VISTA\VI_EMPRESAS\empresa3.php' class='enlace_sidebar'>Empresa 3</a></li>
-<li class='li_sidebar'><a href='<?php echo $dirRoot; ?>MVC_TOCAR\VISTA\VI_EMPRESAS\empresa4.php' class='enlace_sidebar'>Empresa 4</a></li>
-<li class='li_sidebar'><a href='<?php echo $dirRoot; ?>MVC_TOCAR\VISTA\VI_EMPRESAS\empresa5.php' class='enlace_sidebar'>Empresa 5</a></li>
-<li class='li_sidebar'><a href='<?php echo $dirRoot; ?>MVC_TOCAR\VISTA\VI_EMPRESAS\empresa6.php' class='enlace_sidebar'>Empresa 6</a></li>
-<li class='li_sidebar'><a href='<?php echo $dirRoot; ?>MVC_TOCAR\VISTA\VI_EMPRESAS\empresa7.php' class='enlace_sidebar'>Empresa 7</a></li>
+<li class='li_sidebar'><a href=<?php echo $dirRoot; ?>'MVC_TOCAR\VISTA\VI_EMPRESAS\empresa1.php' class='enlace_sidebar'>Empresa 1</a></li>
+<li class='li_sidebar'><a href=<?php echo $dirRoot; ?>'MVC_TOCAR\VISTA\VI_EMPRESAS\empresa2.php' class='enlace_sidebar'>Empresa 2</a></li>
+<li class='li_sidebar'><a href=<?php echo $dirRoot; ?>'MVC_TOCAR\VISTA\VI_EMPRESAS\empresa3.php' class='enlace_sidebar'>Empresa 3</a></li>
+<li class='li_sidebar'><a href=<?php echo $dirRoot; ?>'MVC_TOCAR\VISTA\VI_EMPRESAS\empresa4.php' class='enlace_sidebar'>Empresa 4</a></li>
+<li class='li_sidebar'><a href=<?php echo $dirRoot; ?>'MVC_TOCAR\VISTA\VI_EMPRESAS\empresa5.php' class='enlace_sidebar'>Empresa 5</a></li>
+<li class='li_sidebar'><a href=<?php echo $dirRoot; ?>'MVC_TOCAR\VISTA\VI_EMPRESAS\empresa6.php' class='enlace_sidebar'>Empresa 6</a></li>
+<li class='li_sidebar'><a href=<?php echo $dirRoot; ?>'MVC_TOCAR\VISTA\VI_EMPRESAS\empresa7.php' class='enlace_sidebar'>Empresa 7</a></li>
 
 </ul>";
 
@@ -28,11 +28,11 @@ echo "<p style='font-size:large;'><h4>Somos Asdete</h4></p>
 
 <ul>
 
-<li class='li_sidebar'><a href='<?php echo $dirRoot; ?>MVC_TOCAR\VISTA\VI_UTILS\calendarios.php' class='enlace_sidebar'>Calendarios laborales</a></li>
-<li class='li_sidebar'><a href='<?php echo $dirRoot; ?>MVC_TOCAR\VISTA\VI_UTILS\empleo.php' class='enlace_sidebar'>Tu empleo</a></li>
-<li class='li_sidebar'><a href='<?php echo $dirRoot; ?>MVC_TOCAR\VISTA\VI_UTILS\formacion.php' class='enlace_sidebar'>Forma T</a></li>
-<li class='li_sidebar'><a href='<?php echo $dirRoot; ?>MVC_TOCAR\VISTA\VI_UTILS\prestamos.php' class='enlace_sidebar'>Empresa 4</a></li>
-<li class='li_sidebar'><a href='<?php echo $dirRoot; ?>MVC_TOCAR\VISTA\VI_UTILS\empresa5.php' class='enlace_sidebar'>Empresa 5</a></li>
+<li class='li_sidebar'><a href='<?php echo $dirRoot; ?>'MVC_TOCAR\VISTA\VI_UTILS\calendarios.php' class='enlace_sidebar'>Calendarios laborales</a></li>
+<li class='li_sidebar'><a href='<?php echo $dirRoot; ?>'MVC_TOCAR\VISTA\VI_UTILS\empleo.php' class='enlace_sidebar'>Tu empleo</a></li>
+<li class='li_sidebar'><a href='<?php echo $dirRoot; ?>'MVC_TOCAR\VISTA\VI_UTILS\formacion.php' class='enlace_sidebar'>Forma T</a></li>
+<li class='li_sidebar'><a href='<?php echo $dirRoot; ?>'MVC_TOCAR\VISTA\VI_UTILS\prestamos.php' class='enlace_sidebar'>Empresa 4</a></li>
+<li class='li_sidebar'><a href='<?php echo $dirRoot; ?>'MVC_TOCAR\VISTA\VI_UTILS\afiliacion.php' class='enlace_sidebar'>Empresa 5</a></li>
 
 </ul>";
 
