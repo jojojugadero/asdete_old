df37279a66db4f804d57d8fdafeafb4742b1f4aa
jojojugadero <jmapgrado@gmail.com>
2022-03-14 11:11:15 +0100

    confirmar

--- MVC_TOCAR/CONTROLADOR/convenios.php
+++ MVC_TOCAR/CONTROLADOR/convenios.php
@@ -12,7 +12,7 @@ $incRoot = $_SERVER['DOCUMENT_ROOT'].$dirRoot;
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
  <title>App Personal</title>
-    <link rel="stylesheet" href="MVC_TOCAR\VISTA\ESTILOS\estilos.css">
+ <link rel="stylesheet" href="<?php echo $dirRoot; ?>MVC_TOCAR\VISTA\ESTILOS\estilos.css">
 </head>
 <?php
 
