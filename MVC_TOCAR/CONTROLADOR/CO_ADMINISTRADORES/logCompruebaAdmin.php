ff07370b0bfac194d01f51d3adaac8452ffd6133
jojojugadero <jmapgrado@gmail.com>
2022-03-15 08:23:25 +0100

    confrimado

--- MVC_TOCAR/CONTROLADOR/CO_ADMINISTRADORES/logCompruebaAdmin.php
+++ MVC_TOCAR/CONTROLADOR/CO_ADMINISTRADORES/logCompruebaAdmin.php
@@ -12,7 +12,7 @@ $incRoot = $_SERVER['DOCUMENT_ROOT'].$dirRoot;
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Comprueba Administradores</title>
+    <title>Log Comprueba Administradores</title>
 </head>
 <body>
 
