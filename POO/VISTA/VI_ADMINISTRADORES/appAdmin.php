c8347a629fbba02568ffa0add7ab6ad3720d1e0e
jojojugadero <jmapgrado@gmail.com>
2022-03-16 15:49:37 +0100

    commit

--- POO/VISTA/VI_ADMINISTRADORES/appAdmin.php
+++ POO/VISTA/VI_ADMINISTRADORES/appAdmin.php
@@ -8,103 +8,38 @@ $incRoot = $_SERVER['DOCUMENT_ROOT'].$dirRoot;
 <html lang="es">
 <?php
 
-include $incRoot.'MVC_TOCAR/MODELO/datos.php';
+include $incRoot.'POO/MODELO/datos.php';
+include $incRoot.'POO/CONTROLADOR/CO_ADMINISTRADORES/appAdministra.php';
 
 
 //Iniciamos sesión
 session_start();
 
+$dat = new Datos();
+$appAd = new AppAdministra();
+
 //Comprobamos que la sesión es correcta y si es correcta se queda en la página se queda en la página y si no, nos redirige a index.php 
-if(isset($_SESSION['personal_sesion']) == 'personal_sesion') {
-    //$url1 = $dirRoot."MVC_TOCAR\VISTA\ADMINISTRADORES\appAdmin.php";
-    //header('Location: '.$url1);
-  } else {
-    $url2 =$dirRoot."MVC_TOCAR/VISTA/index.php";
-    header('Location: '.$url2);
-  }
+
+    $appAd->compruebaSession();
+
 
   //Recogemos las variables cuando insertamos nuevos registros, modificamos o eliminamos
-  $id =  isset($_POST['id']) ? $_POST['id'] : '';
-  $nif =  isset($_POST['nif']) ? $_POST['nif'] : '';
-  $password =  isset($_POST['password']) ? $_POST['password'] : '';
-  $nombre =  isset($_POST['nombre']) ? $_POST['nombre'] : '';
-  $ape1 =  isset($_POST['ape1']) ? $_POST['ape1'] : '';
-  $ape2 =  isset($_POST['ape2']) ? $_POST['ape2'] : '';
-  $telefono =  isset($_POST['telefono']) ? $_POST['telefono'] : '';
-  $email =  isset($_POST['email']) ? $_POST['email'] : '';
-  $direccion =  isset($_POST['direccion']) ? $_POST['direccion'] : '';
-  $id_empresa =  isset($_POST['empresa']) ? $_POST['empresa'] : '';
+      $appAd->varCrud();
   
   //Recogemos variables para la acción que va a hacer el botón en el onclick
-  $swinsertar =  isset($_POST['swinsertar']) ? $_POST['swinsertar'] : '';
-  $swmodificar =  isset($_POST['swmodificar']) ? $_POST['swmodificar'] : '';
-  $swmodificarapply =  isset($_POST['swmodificarapply']) ? $_POST['swmodificarapply'] : '';
-  $sweliminar =  isset($_POST['sweliminar']) ? $_POST['sweliminar'] : '';
+     $appAd->varOnClick();
 
   //Recogemos el valor de las variables para realizar las operaciones de base de datos
-  $datos['id'] = $id;
-  $datos['nif'] = $nif;
-  $datos['password'] = $password;
-  $datos['nombre'] = $nombre;
-  $datos['apellido1'] = $ape1;
-  $datos['apellido2'] = $ape2;
-  $datos['telefono'] = $telefono;
-  $datos['email'] = $email;
-  $datos['direccion'] = $direccion;
-  $datos['id_empresa_fk'] = $id_empresa;
+ 
 
   //Si vamos a modificar el afiliado se recoge el afiliado por ID para su modificación
-  $afil_modi = $swmodificar == 'S' ? getAfiliado($id) : $datos;
+  $afil_modi = $swmodificar == 'S' ? $datos->getAfiliado($id) : $datos;
 
   //Se comprueba el tipo de acción para dar de alta modificar o eliminar el afiliado
-  $msgValidacion = "";
-  if($swinsertar == 'S' || $swmodificarapply == 'S') {
-    if(trim($nif) == '') {
-      $msgValidacion = "El NIF es obligatorio";
-    } else if(trim($password) == '') {
-      $msgValidacion = "La contraseña es obligatoria";
-    } else if(trim($nombre) == '') {
-      $msgValidacion = "El nombre es obligatorio";
-    } else if(trim($ape1) == '') {
-      $msgValidacion = "El apellido 1 es obligatorio";
-    } else if(trim($ape2) == '') {
-      $msgValidacion = "El apellido 2 es obligatorio";
-    } else if(trim($telefono) == '') {
-      $msgValidacion = "El teléfono es obligatorio";
-    } else if(trim($email) == '') {
-      $msgValidacion = "El email es obligatorio";
-    } else if(trim($direccion) == '') {
-      $msgValidacion = "La dirección es obligatorio";
-    } else if(trim($id_empresa) == '') {
-      $msgValidacion = "La empresa es obligatorio";
-    }
-    if(trim($nif) != '') {
-      $afil_exist = getAfiliadoNIF($nif);
-      $id_exist = isset($afil_exist['id']) ? $afil_exist['id'] : '';
-      if($id_exist > 0 && $id != $id_exist) {
-        $msgValidacion = "Ya existe un afiliado con ese NIF.";
-      }
-    }
-    if(trim($msgValidacion) != "") {
-      $afil_modi = $datos;
-    }
-  }
-  if(trim($msgValidacion) == "" && $swmodificarapply == 'S') {
-    $swmodificar = "N";
-  }
-  if(trim($msgValidacion) == "") {
-    if($swinsertar == 'S') {
-      altaAfiliado($datos);
-    } else if($swmodificarapply == 'S') {
-      modAfiliado($datos);
-    } else if($sweliminar == 'S') {
-      eliminarAfiliado($id);
-    }
-  }
-
+ 
   //Recogemos todos los afiliados y empresas para mostarlos por pantalla
-  $afiliados = getAfiliados();
-  $empresas = getEmpresas();
+  $afiliados = $datos->getAfiliados();
+  $empresas = $datos->getEmpresas();
 
 ?>
 
