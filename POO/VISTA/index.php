5799f6a009d77c038ebb6f860b62d977069643b7
jojojugadero <jmapgrado@gmail.com>
2022-04-04 20:33:33 +0200

    commit

--- POO/VISTA/index.php
+++ POO/VISTA/index.php
@@ -29,7 +29,7 @@ $incRoot = $_SERVER['DOCUMENT_ROOT'].$dirRoot;
     
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<?php   //  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>?>
     <link rel="stylesheet" href="<?php echo $dirRoot; ?>POO/VISTA/ESTILOS/estilos.css">
 </head>
 
